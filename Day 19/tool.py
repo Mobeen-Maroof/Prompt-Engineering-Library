00b79bb3ab67343d29863354cdadd095c140c05d
Mobeen-Maroof <mohbeenmaroof45@gmail.com>
2026-09-09 18:16:01 +0500

    Update tool.py

--- Day 19/tool.py
+++ Day 19/tool.py
@@ -1,17 +1,10 @@
 from datetime import datetime
 
-def calculator(expression):
-    """
-    Evaluates a mathematical expression.
-    """
+def calculator(expression: str):
     try:
-        return eval(expression)
-    except Exception:
-        return "Invalid Expression"
+        return str(eval(expression))
+    except Exception as e:
+        return f"Error: {e}"
 
-
-def get_current_time():
-    """
-    Returns the current system time.
-    """
+def current_time():
     return datetime.now().strftime("%I:%M:%S %p")
